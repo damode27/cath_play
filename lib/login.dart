a2618c4d563cf0969e8b92c5706546bc259eceb2
Daniel Morales <122396590+damode27@users.noreply.github.com>
2023-04-12 13:24:05 -0600

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -15,15 +15,11 @@ class Login extends StatefulWidget{
 }
 
 class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _passwordTextController = TextEditingController();
-  final TextEditingController _userTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      //appBar: AppBar(title: const Text("Hola mundo"),),
       body: MyBodyContainer(
         widget: Padding(
           padding: EdgeInsets.fromLTRB(
@@ -31,7 +27,6 @@ class _LoginState extends State<Login> {
           ),
           child: Column(
             children: [
-              //logoWidget("assets/images/logo.png", 300),
               const MyTitleText(text: "Bienvenido(a) a", size: 24),
               const SizedBox(height: 5,),
               const MyTitleText(text: "Cath Play", size: 40),
@@ -46,31 +41,6 @@ class _LoginState extends State<Login> {
               MyGradientButton(buttonText: "ENTRAR CON GOOGLE", onPressed: (){
                 googleLogin();
               }),
-
-              /*
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      MyTextFormField(labelText: "Email", icon: Icons.mail, controller: _userTextController, isPassword: false),
-                      const SizedBox(height: 20,),
-                      MyTextFormField(labelText: "Contraseña", icon: Icons.lock, controller: _passwordTextController, isPassword: true),
-                      const SizedBox(height: 20,),
-                      MyGradientButton(buttonText: "ENTRAR", onPressed: (){
-                        if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          //iniciarSesion(context);
-                        }
-                      }),
-                      const SizedBox(height: 40,),
-                      const MyContentText(text: "¿No tienes una cuenta? Crea una aqui:", size: 14),
-                      const SizedBox(height: 5,),
-                      MyGradientButton(buttonText: "REGISTRARME", onPressed: (){}),
-                    ],
-                  ),
-                ),
-
-                 */
 
               const SizedBox(height: 40.0,),
 
